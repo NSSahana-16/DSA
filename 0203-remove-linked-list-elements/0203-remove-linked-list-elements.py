1a31a4cb03f06fc2b76e6d7b72f741af37cd62c3
Sahana N S <sahanans.shivashankar@gmail.com>
2026-03-16 12:03:03 +0530

    0203-remove-linked-list-elements.c

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -3,18 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeElements(self, head, val):
-        dummy_head = ListNode(-1)
-        dummy_head.next = head
-        
-        current_node = dummy_head
-        while current_node.next != None:
-            if current_node.next.val == val:
-                current_node.next = current_node.next.next
-            else:
-                current_node = current_node.next
-                
-        return dummy_head.next
+struct ListNode* removeElements(struct ListNode* head, int val) {
+   
+    while(head != NULL && head->val==val){
+        head=head->next;
+    }
+    struct ListNode* temp=head;
+    while(temp!=NULL && temp->next!=NULL)
+    {
+        if(temp->next->val==val)
+        {
+            temp->next=temp->next->next;
+        }
+        else
+        {
+        temp=temp->next;
+        }
+
+    }
+    return head;
+    
+}
         
         
